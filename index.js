a3ce85b78046a9b2dcc101f424f939d52d8e9c97
DloperHassan <nahidis123@outlook.com>
2025-10-04 22:27:54 +0600

    Load Pets Data And Photo

--- index.js
+++ index.js
@@ -23,6 +23,10 @@ async function petsArea() {
     ptetsA.forEach( (pet) => {
         const clone = template.content.cloneNode(true) 
         clone.querySelector('h3').textContent = pet.name
+        clone.querySelector('.pet-description').textContent = pet.description
+        clone.querySelector('.pet-age').textContent = createAgeText(pet.birthYear)
+        clone.querySelector('.potosss').src = pet.photo
+        clone.querySelector('.potosss').alt = `A ${pet.species} named ${pet.name}`
         wrapper.appendChild(clone)
     });
     document.querySelector('.list-Of-pets').appendChild(wrapper)
@@ -30,6 +34,16 @@ async function petsArea() {
 }
 
 petsArea() // fuction call 
+
+function createAgeText(birthYear) {
+    const currentYear = new Date().getFullYear()
+    const  age = currentYear - birthYear
+
+    if(age == 1) return "1 year old"
+    if(age == 0) return "Less then a year old"
+
+    return age + " Years old"
+}
 
 
 
